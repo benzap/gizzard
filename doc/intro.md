40db16f658ade0c583ce88b8a2885d23bcb02a44
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-08-26 01:40:18 -0400

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gizzard
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gizzard/core_test.clj
@@ -0,0 +1,7 @@
+(ns gizzard.core-test
+  (:require [clojure.test :refer :all]
+            [gizzard.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
